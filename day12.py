7d91c647e426b1bb59321f3a796ecccbeb615b1a
dissolve <dissolve1882@naver.com>
2025-02-25 17:29:22 +0900

    day12 homework1

--- day12.py
+++ day12.py
@@ -1,9 +1,21 @@
-# Assignment
-# v0.9) v0.8 파일의 결측치 값을 산술평균으로 채워 넣는 다양한 방법을 적용하시오.
 import numpy as np
 import pandas as pd
-from sklearn.impute import SimpleImputer
 
+# pandas 사용
+df = pd.DataFrame(
+    {
+        'A': [1, 2, np.nan, 4],
+        'B': [np.nan, 12, 3, 4],
+        'C': [1, 2, 3, 4]
+    }
+)
+
+# 평균값으로 결측치 채우기
+df_mean_filled = df.fillna(df.mean())
+print(df_mean_filled)
+
+
+from sklearn.impute import SimpleImputer
 df = pd.DataFrame(
     {
         'A':[1, 2, np.nan, 4],
@@ -12,3 +24,34 @@ df = pd.DataFrame(
     }
 )
 print(df)
+i = SimpleImputer(strategy='mean')
+df[['A', 'B']] = i.fit_transform(df[['A', 'B']])
+print(df)
+
+
+# apply()와 lambda 사용
+df = pd.DataFrame(
+    {
+        'A': [1, 2, np.nan, 4],
+        'B': [np.nan, 12, 3, 4],
+        'C': [1, 2, 3, 4]
+    }
+)
+
+df_lambda_filled = df.apply(lambda x: x.fillna(x.mean()), axis=0)
+
+print(df_lambda_filled)
+
+# transform() 사용
+df = pd.DataFrame(
+    {
+        'A': [1, 2, np.nan, 4],
+        'B': [np.nan, 12, 3, 4],
+        'C': [1, 2, 3, 4]
+    }
+)
+
+df_transform_filled = df.copy()
+df_transform_filled[df.columns] = df.transform(lambda x: x.fillna(x.mean()))
+
+print(df_transform_filled)
